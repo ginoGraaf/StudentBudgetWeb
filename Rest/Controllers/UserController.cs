61bb0bdff8a9186358999e42f5ca7071e19e0a81
bramvanderzee <bram.vdzee@gmail.com>
2020-03-31 15:29:43 +0200

    Added put and delete functions, added comments

--- Rest/Controllers/UserController.cs
+++ Rest/Controllers/UserController.cs
@@ -9,6 +9,7 @@ namespace Rest.Controllers {
     public class UserController: ControllerBase {
         [HttpGet]
         public IEnumerable<User> Get() {
+            // Get all users
             return new List<User>{
                 new User{Id = 0, Name = "Joeri"},
                 new User{Id = 42, Name = "Bram"}
@@ -18,13 +19,27 @@ namespace Rest.Controllers {
         [HttpGet]
         [Route("{Id}")]
         public User Get(int Id) {
+            // Get a single user by id
             return new User{Id = Id, Name = "Bram"};
         }
 
         [HttpPost]
         public User Post(User user) {
+            // Create the user
             user.Id = 42;
             return user;
+        }
+
+        [HttpPut]
+        public User Put(User user) {
+            // Do something to update the user at id=user.Id
+            return user;
+        }
+
+        [HttpDelete]
+        [Route("{Id}")]
+        public void Delete(int Id) {
+            // Delete user
         }
     }
 }
