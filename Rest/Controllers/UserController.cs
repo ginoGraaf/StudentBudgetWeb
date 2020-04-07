3741a67f583e8115dd437f0e0c772d3988820513
bramvanderzee <bram.vdzee@gmail.com>
2020-04-07 13:08:57 +0200

    Worked on user rest, does not work yet but added logging

--- Rest/Controllers/UserController.cs
+++ Rest/Controllers/UserController.cs
@@ -2,37 +2,58 @@ using System;
 using Rest.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
 
 namespace Rest.Controllers {
     [ApiController]
     [Route("api/User/")]
     public class UserController: ControllerBase {
+
+        // Sample user database
+        private List<User> users = new List<User> {
+            new User{Id = 0, Name = "Joeri"},
+            new User{Id = 1, Name = "Siora"},
+            new User{Id = 2, Name = "Bram"}
+        };
+        private int Index = 3;
+
+        
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         public IEnumerable<User> Get() {
             // Get all users
-            return new List<User>{
-                new User{Id = 0, Name = "Joeri"},
-                new User{Id = 42, Name = "Bram"}
-                };
+            return users;
         }
 
         [HttpGet]
         [Route("{Id}")]
         public User Get(int Id) {
             // Get a single user by id
-            return new User{Id = Id, Name = "Bram"};
+            return users.FirstOrDefault(u => u.Id == Id);
         }
 
         [HttpPost]
-        public User Post(User user) {
+        public User Post([FromBody] User user) {
             // Create the user
-            user.Id = 42;
+            user.Id = Index;
+            Index++;
+            users.Add(user);
             return user;
         }
 
         [HttpPut]
         public User Put(User user) {
             // Do something to update the user at id=user.Id
+            var oldUser = users.FirstOrDefault(u => u.Id == user.Id);
+            users.Remove(oldUser);
+            users.Add(user);
             return user;
         }
 
@@ -40,6 +61,7 @@ namespace Rest.Controllers {
         [Route("{Id}")]
         public void Delete(int Id) {
             // Delete user
+            users.Remove(users.FirstOrDefault(u => u.Id == Id));
         }
     }
 }
