e6a7e266341f4c8ee97a9e9d21a0f7192660524c
JoeriRietjens <45871940+JoeriRietjens@users.noreply.github.com>
2020-04-14 15:19:55 +0200

    Updated the DB
    Boejuh -Bram

--- DataAccessLibrary/Migrations/20200414130450_InitialDbMigration.Designer.cs
+++ DataAccessLibrary/Migrations/20200414130450_InitialDbMigration.Designer.cs
@@ -3,14 +3,16 @@ using DataAccessLibrary;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLibrary.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20200414130450_InitialDbMigration")]
+    partial class InitialDbMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
