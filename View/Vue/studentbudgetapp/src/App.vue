fdfc94272d2ec03b774930f7d5b2a1b7e0149562
Gino <ginovandegraaf@hotmail.com>
2020-04-14 15:42:19 +0200

    de pie chart is nu een aparte component

--- View/Vue/studentbudgetapp/src/App.vue
+++ View/Vue/studentbudgetapp/src/App.vue
@@ -1,30 +1,20 @@
 <template>
   <div id="app">
-    
-    <pie :pieData="pieData"/>
+    <PieComponent v-bind="piedata" ></PieComponent>
+   
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-import Pie from './components/Pie'
+import PieComponent from './components/PieComponent'
 export default {
   name: 'App',
-  data:()=>
-  {
-    return{
-      pieData:[{color:'#0B6487',value:10},
-      {color:'#9D1F37',value:40},
-      {color:'#F6931C',value:30},
-      {color:'#9A6487',value:10},
-      {color:'#58A384',value:10}]
-    };
+  components:{
+    PieComponent,
   },
-components:
-  {
-    Pie
-  }
+
 }
+
 </script>
 
 <style lang="css">
---
+++ View/Vue/studentbudgetapp/src/components/PieComponent.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <h1>Hello World</h1>
+   <pie :pieData="pieData"/>
+</div>
+</template>
+
+<script>
+
+import Pie from './Pie'
+
+export default {
+    components:{
+        Pie
+    },
+  data:()=>
+  {
+    return{
+      pieData:[{color:'#0B6487',value:10},
+      {color:'#9D1F37',value:40},
+      {color:'#F6931C',value:30},
+      {color:'#9A6487',value:10},
+      {color:'#58A384',value:10}]
+    };
+  },
+
+}
+</script>
