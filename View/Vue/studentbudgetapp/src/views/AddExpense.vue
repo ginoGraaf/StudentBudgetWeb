97adbe6d9377e60812562bff69b45e9b149a685c
SioravW <SioravW@gmail.com>
2020-04-15 09:17:21 +0200

    chooseCategory not working

--- View/Vue/studentbudgetapp/src/views/AddExpense.vue
+++ View/Vue/studentbudgetapp/src/views/AddExpense.vue
@@ -7,16 +7,18 @@
       <input type="number" step="0.01" placeholder="0,00" v-bind="amount">
     </div>
     <div>
+      <input type="checkbox" v-bind="Weekly"><span>Weekly</span>
       <input type="checkbox" v-bind="Monthly"><span>Monthly</span>
+      <input type="checkbox" v-bind="Annual"><span>Annual</span>
     </div>
     <div>
-      <ChooseCategory />
+      <ChooseCategory v-bind:ChooseCategory="ChooseCategory"/>
     </div>
   </div>
 </template>
 
 <script> 
-//import ChooseCategory form '../components/ChooseCategory.vue'
+import ChooseCategory from '../components/ChooseCategory.vue';
 
 export default {
   name: 'AddExpense'
