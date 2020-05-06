a1deaf7f622ed2b31e8a351ce286c288f14ad542
bramvanderzee <bram.vdzee@gmail.com>
2020-05-06 10:43:03 +0200

    Added more vuex stores

--- View/Vue/studentbudgetapp/src/services/user.js
+++ View/Vue/studentbudgetapp/src/services/user.js
@@ -1,5 +1,5 @@
-import config from 'config';
-import { authHeader } from '../helpers';
+import config from 'config'
+import { authHeader } from '../helpers'
 
 export const userService = {
     login,
@@ -9,14 +9,14 @@ export const userService = {
     getById,
     update,
     delete: _delete
-};
+}
 
 function login(username, password) {
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
-    };
+    }
 
     return fetch(`${config.apiUrl}/users/authenticate`, requestOptions)
         .then(handleResponse)
@@ -24,16 +24,16 @@ function login(username, password) {
             // login successful if there's a jwt token in the response
             if (user.token) {
                 // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('user', JSON.stringify(user));
+                localStorage.setItem('user', JSON.stringify(user))
             }
 
-            return user;
-        });
+            return user
+        })
 }
 
 function logout() {
     // remove user from local storage to log user out
-    localStorage.removeItem('user');
+    localStorage.removeItem('user')
 }
 
 function register(user) {
@@ -41,18 +41,18 @@ function register(user) {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(user)
-    };
+    }
 
-    return fetch(`${config.apiUrl}/users/register`, requestOptions).then(handleResponse);
+    return fetch(`${config.apiUrl}/users/register`, requestOptions).then(handleResponse)
 }
 
 function getAll() {
     const requestOptions = {
         method: 'GET',
         headers: authHeader()
-    };
+    }
 
-    return fetch(`${config.apiUrl}/users`, requestOptions).then(handleResponse);
+    return fetch(`${config.apiUrl}/users`, requestOptions).then(handleResponse)
 }
 
 
@@ -60,9 +60,9 @@ function getById(id) {
     const requestOptions = {
         method: 'GET',
         headers: authHeader()
-    };
+    }
 
-    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
+    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse)
 }
 
 function update(user) {
@@ -70,9 +70,9 @@ function update(user) {
         method: 'PUT',
         headers: { ...authHeader(), 'Content-Type': 'application/json' },
         body: JSON.stringify(user)
-    };
+    }
 
-    return fetch(`${config.apiUrl}/users/${user.id}`, requestOptions).then(handleResponse);
+    return fetch(`${config.apiUrl}/users/${user.id}`, requestOptions).then(handleResponse)
 }
 
 // prefixed function name with underscore because delete is a reserved word in javascript
@@ -80,25 +80,25 @@ function _delete(id) {
     const requestOptions = {
         method: 'DELETE',
         headers: authHeader()
-    };
+    }
 
-    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
+    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse)
 }
 
 function handleResponse(response) {
     return response.text().then(text => {
-        const data = text && JSON.parse(text);
+        const data = text && JSON.parse(text)
         if (!response.ok) {
             if (response.status === 401) {
                 // auto logout if 401 response returned from api
-                logout();
-                location.reload(true);
+                logout()
+                location.reload(true)
             }
 
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
+            const error = (data && data.message) || response.statusText
+            return Promise.reject(error)
         }
 
-        return data;
-    });
+        return data
+    })
 }
