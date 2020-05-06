b40e5508b2609249733a38ea9e1079ac54d4fae2
bramvanderzee <bram.vdzee@gmail.com>
2020-05-06 11:34:00 +0200

    routes prettier

--- View/Vue/studentbudgetapp/src/router/index.js
+++ View/Vue/studentbudgetapp/src/router/index.js
@@ -1,38 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
 import LoginPage from '../views/LoginPage.vue'
 import RegisterPage from '../views/RegisterPage.vue'
+import AddExpense from '../views/AddExpense.vue'
 
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  { path: '/login', component: LoginPage},
-  { path: '/register', RegisterPage },
-
-  {
-    path: '/AddExpense',
-    name: 'AddExpense',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AddExpense.vue')
-  }
-
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/login', name: 'Login', component: LoginPage},
+  { path: '/register', name: 'Register', RegisterPage },
+  { path: '/AddExpense', name: 'AddExpense', component: AddExpense }
 ];
 
 const router = new VueRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, next) => {
   // redirect to login page if not logged in and trying to access a restricted page
   const publicPages = ['/login', '/register'];
   const authRequired = !publicPages.includes(to.path);
@@ -45,5 +32,3 @@ router.beforeEach((to, from, next) => {
   next();
 }); 
 
-
-export default router
