fcd56514ab8f7260aa4513eae6df77177835afe4
bramvanderzee <bram.vdzee@gmail.com>
2020-05-06 10:17:09 +0200

    added vuex

--- View/Vue/studentbudgetapp/src/main.js
+++ View/Vue/studentbudgetapp/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.Use(Vuex)
 
 new Vue({
+  store,
   router,
   render: h => h(App)
 }).$mount('#app')
