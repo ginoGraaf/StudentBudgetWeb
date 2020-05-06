90fcd61b87796389352a034078d1615b9148c936
bramvanderzee <bram.vdzee@gmail.com>
2020-05-06 10:47:36 +0200

    added register and login pages

--- View/Vue/studentbudgetapp/src/router/index.js
+++ View/Vue/studentbudgetapp/src/router/index.js
@@ -14,8 +14,8 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
-  { path: '/login', component: LoginPage },
-  { path: '/register', component: RegisterPage },
+  { path: '/views/login', component: LoginPage },
+  { path: '/views/register', component: RegisterPage },
 
   {
     path: '/AddExpense',
