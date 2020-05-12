e983b8c2ab0fb45005ab2c08464c99964ffb743b
SanderWieringa <43203835+SanderWieringa@users.noreply.github.com>
2020-05-12 13:13:59 +0200

    deleted responsibility

--- DataAccessLibrary/Models/Expense.cs
+++ DataAccessLibrary/Models/Expense.cs
@@ -12,14 +12,5 @@ namespace DataAccessLibrary
         public int Id { get; set; }
         public string Name { get; set; }
         public int Amount { get; set; }
-        public Category Category { get; set; }
-
-        public Expense(int id, string name, int amount, Category category)
-        {
-            Id = id;
-            Name = name;
-            Amount = amount;
-            Category = category;
-        }
     }
 }
