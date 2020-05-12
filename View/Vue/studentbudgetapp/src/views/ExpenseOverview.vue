14db9ca220be029ced43e92d2c550ac2cf9afb1c
JoeriRietjens <45871940+JoeriRietjens@users.noreply.github.com>
2020-05-12 14:55:14 +0200

    Expense overview

---
+++ View/Vue/studentbudgetapp/src/views/ExpenseOverview.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="Expenses"> 
+        <li v-for="Expense in Expenses" :key="Expense.id">
+           Expense: {{expense.name}} | Amount: {{expense.amount}}
+        </li>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name: ExpenseOverview,
+    
+    data(){
+        return{
+           Expenses:[] 
+        }       
+    },
+    Methods: {
+
+        async created(){   
+            try{
+               const res = await axios.get("URL");
+            this.Expenses = res.data; 
+            }
+            catch(e){
+                console.error(e);
+            }
+        }
+    }
+
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
