52532a9b2c36e6dda7f08f07b1c370b3ffd1295b
JoeriRietjens <45871940+JoeriRietjens@users.noreply.github.com>
2020-05-12 14:53:14 +0200

    Added Expense Page

--- View/Vue/studentbudgetapp/src/App.vue
+++ View/Vue/studentbudgetapp/src/App.vue
@@ -3,8 +3,10 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/AddExpense">AddExpense</router-link> | 
+      <router-link to="/ExpenseOverview"> Expense Overview</router-link> |
       <router-link to="/login">Login</router-link> | 
       <router-link to="/register">Register</router-link>
+      
     </div>
     <router-view/>
   </div>
