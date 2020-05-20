c594279c3468a23fcacdb5a6ac03e0e770de01ba
Gino <ginovandegraaf@hotmail.com>
2020-05-20 11:55:54 +0200

    controller is er maar geen database
    Joeri waar is mijn databse?

---
+++ DataAccessLibrary/Models/SavingTargetModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary
+{
+    public class SavingTargetModel
+    {
+        public int ID { get; set; }
+        public string SavingTargetName { get; set; }
+        public int GoalAmount { get; set; }
+    }
+}
---
+++ Rest/Controllers/SavingTargetController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DataAccessLibrary;
+
+namespace Rest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SavingTargetController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public SavingTargetController(ApplicationContext context)
+        {
+            this._context = context;
+        }
+        // GET: api/SavingTarget/5
+        [HttpGet("{id}", Name = "Get")]
+        public SavingTargetModel Get(int id)
+        {
+            return new SavingTargetModel();
+        }
+
+        // POST: api/SavingTarget
+        [HttpPost]
+        public void Post([FromBody] SavingTargetModel savingTarget)
+        {
+            
+        }
+
+        // PUT: api/SavingTarget/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] SavingTargetModel savingTarget)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
