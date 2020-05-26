d2069b0a27cc42e4f831c5656263d68f3075379d
Maryam Bagdady <I338331@fhict.nl>
2020-05-26 10:35:56 +0200

    average expenses

--- DataAccessLibrary/Models/Expense.cs
+++ DataAccessLibrary/Models/Expense.cs
@@ -10,6 +10,6 @@ namespace DataAccessLibrary
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Amount { get; set; }
+        public int Amount { get; set; } 
     }
 }
