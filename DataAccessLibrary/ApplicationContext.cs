78741db4441357361b0cec700f2124b7d8da8c01
SanderWieringa <43203835+SanderWieringa@users.noreply.github.com>
2020-06-02 09:35:23 +0200

    update applicationcontext

--- DataAccessLibrary/ApplicationContext.cs
+++ DataAccessLibrary/ApplicationContext.cs
@@ -12,23 +12,5 @@ namespace DataAccessLibrary
         public DbSet<Expense> Expenses { get; set; }
         public DbSet<Category> Categories { get; set; }
 
-        public double CalculateAverage(DbSet<Expense> expenses)
-        {
-            double amount = expenses.Count();
-            double average;
-
-            average = TotalExpenses(expenses) / amount;
-            return average;
-        }
-
-        private double TotalExpenses(DbSet<Expense> expenses)
-        {
-            double TotalExpenses = 0;
-            foreach (var item in expenses)
-            {
-                totalExpenses += item.Amount;
-            }
-            return TotalExpenses;
-        }
     }
 }
