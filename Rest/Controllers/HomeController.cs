ce6451000a8691a6c081abf26f70a1ccc9351d22
Maryam Bagdady <I338331@fhict.nl>
2020-06-09 10:49:04 +0200

    average edition

--- Rest/Controllers/HomeController.cs
+++ Rest/Controllers/HomeController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessLibrary;
+using DataAccessLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Rest.Models;
 
@@ -11,6 +14,10 @@ namespace Rest.Controllers
 {
     public class HomeController : Controller
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -32,6 +39,25 @@ namespace Rest.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+        }
+        public double CalculateAverage(DbSet<Expense> expenses)
+        {
+            double amount = expenses.Count();
+            double average;
+
+            average = TotalExpenses(expenses) / amount;
+            return average;
+        }
+
+        private double TotalExpenses(DbSet<Expense> expenses)
+        {
+            double TotalExpenses = 0;
+            foreach (var item in expenses)
+            {
+               TotalExpenses += (double)item.Amount;
+            }
+            return TotalExpenses;
         }
     }
 }
