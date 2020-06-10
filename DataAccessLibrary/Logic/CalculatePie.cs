fad329a120cf87f460b8114f6449bbfcbc47ba89
Gino <ginovandegraaf@hotmail.com>
2020-06-10 10:20:16 +0200

    PieData calculation updated
    moet nog een keer getest worden met data

--- DataAccessLibrary/Logic/CalculatePie.cs
+++ DataAccessLibrary/Logic/CalculatePie.cs
@@ -18,33 +18,39 @@ namespace DataAccessLibrary.Logic
         //later make this user ID.
         public List<PieModel>GetPieData()
         {
-            List<Category> category = new List<Category>();
-            category = application.Categories.ToList();
+            List<Expense> expenses = new List<Expense>();
+            expenses = application.Expenses.ToList();
+         
+            decimal amount = GetTotalAmount(expenses);
+
+            return CreateData(expenses,amount);
+        }
+
+        List<PieModel>CreateData(List<Expense> expenses,decimal amount)
+        {
             Random rnd;
-            double amount = GetTotalAmount(category);
             List<PieModel> pieData = new List<PieModel>();
-            for (int i = 0; i < category.Count; i++)
+            for (int i = 0; i < expenses.Count; i++)
             {
                 double Precentage = 0;
                 rnd = new Random(new System.DateTime().Millisecond);
-                string colorName = Color.FromArgb(rnd.Next(0,255), rnd.Next(0, 255), rnd.Next(0, 255)).ToString();
-                if (category[i].Bedrag > 0.00d)
+                string colorName = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255)).ToString();
+                if (expenses[i].Amount > 0.00m)
                 {
-                    Precentage = (category[i].Bedrag / amount) * 100;
+                    Precentage = ((double)expenses[i].Amount / (double)amount) * 100;
                 }
-       
-                PieModel pieDataInfo = new PieModel(category[i].Title, colorName, (int)Precentage);
+                PieModel pieDataInfo = new PieModel(expenses[i].Category.Title, colorName, (int)Precentage);
                 pieData.Add(pieDataInfo);
             }
             return pieData;
         }
 
-        double GetTotalAmount(List<Category> category)
+        decimal GetTotalAmount(List<Expense> expenses)
         {
-            double amount = 0;
-            for (int i = 0; i < category.Count; i++)
+            decimal amount = 0;
+            for (int i = 0; i < expenses.Count; i++)
             {
-                amount += category[i].Bedrag;   
+                amount += expenses[i].Amount;   
             }
             return amount;
         }
