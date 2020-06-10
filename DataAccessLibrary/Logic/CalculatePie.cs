e25cc079480e1448fd0e722005a29e6019f3ba8d
Gino <ginovandegraaf@hotmail.com>
2020-06-10 15:22:34 +0200

    fix pie chart

--- DataAccessLibrary/Logic/CalculatePie.cs
+++ DataAccessLibrary/Logic/CalculatePie.cs
@@ -18,39 +18,36 @@ namespace DataAccessLibrary.Logic
         //later make this user ID.
         public List<PieModel>GetPieData()
         {
-            List<Expense> expenses = new List<Expense>();
-            expenses = application.Expenses.ToList();
-         
-            decimal amount = GetTotalAmount(expenses);
-
-            return CreateData(expenses,amount);
+            List<Category> category = new List<Category>();
+            category = application.Categories.ToList();
+            return CreateData(category, GetTotalAmount(category));
         }
 
-        List<PieModel>CreateData(List<Expense> expenses,decimal amount)
+        List<PieModel>CreateData(List<Category> category, double amount)
         {
             Random rnd;
             List<PieModel> pieData = new List<PieModel>();
-            for (int i = 0; i < expenses.Count; i++)
+            for (int i = 0; i < category.Count; i++)
             {
                 double Precentage = 0;
                 rnd = new Random(new System.DateTime().Millisecond);
                 string colorName = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255)).ToString();
-                if (expenses[i].Amount > 0.00m)
+                if (category[i].Bedrag > 0.00d)
                 {
-                    Precentage = ((double)expenses[i].Amount / (double)amount) * 100;
+                    Precentage = ((double)category[i].Bedrag / (double)amount) * 100;
                 }
-                PieModel pieDataInfo = new PieModel(expenses[i].Category.Title, colorName, (int)Precentage);
+                PieModel pieDataInfo = new PieModel(category[i].Title, colorName, (int)Precentage);
                 pieData.Add(pieDataInfo);
             }
             return pieData;
         }
 
-        decimal GetTotalAmount(List<Expense> expenses)
+        double GetTotalAmount(List<Category> category)
         {
-            decimal amount = 0;
-            for (int i = 0; i < expenses.Count; i++)
+            double amount = 0;
+            for (int i = 0; i < category.Count; i++)
             {
-                amount += expenses[i].Amount;   
+                amount += category[i].Bedrag;
             }
             return amount;
         }
