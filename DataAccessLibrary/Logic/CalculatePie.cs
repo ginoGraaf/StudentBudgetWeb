1072fc2c9edfa83aa0f5ea6293a806dc5c672c97
Siora van Weperen <428439@student.fontys.nl>
2020-06-16 15:39:35 +0200

    Update CalculatePie.cs

--- DataAccessLibrary/Logic/CalculatePie.cs
+++ DataAccessLibrary/Logic/CalculatePie.cs
@@ -11,50 +11,50 @@ namespace DataAccessLibrary.Logic
 {
     public class CalculatePie
     {
-        ApplicationContext application;
-        public CalculatePie(ApplicationContext application)
-        {
-            this.application = application;
-        }
+        //ApplicationContext application;
+        //public CalculatePie(ApplicationContext application)
+        //{
+        //    this.application = application;
+        //}
 
-        //later make this user ID.
-        public async Task<IEnumerable<PieModel>>GetPieData( int userID)
-        {
-            List<Category> category = new List<Category>();
-            category = await application.Categories.Where(c => c.UserId == userID).ToListAsync();
-            List<PieModel> pieData = new List<PieModel>();
-            pieData =CreateData(category, GetTotalAmount(category));
-            return pieData;
-        }
+        ////later make this user ID.
+        //public async Task<IEnumerable<PieModel>>GetPieData( int userID)
+        //{
+        //    List<Category> category = new List<Category>();
+        //    category = await application.Categories.Where(c => c.UserId == userID).ToListAsync();
+        //    List<PieModel> pieData = new List<PieModel>();
+        //    pieData =CreateData(category, GetTotalAmount(category));
+        //    return pieData;
+        //}
 
-        List<PieModel>CreateData(List<Category> category, double amount)
-        {
-            Random rnd;
-            List<PieModel> pieData = new List<PieModel>();
-            for (int i = 0; i < category.Count; i++)
-            {
-                decimal Precentage = 0;
-                rnd = new Random(new System.DateTime().Millisecond);
-                string colorName = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255)).ToString();
+        //List<PieModel>CreateData(List<Category> category, double amount)
+        //{
+        //    Random rnd;
+        //    List<PieModel> pieData = new List<PieModel>();
+        //    for (int i = 0; i < category.Count; i++)
+        //    {
+        //        decimal Precentage = 0;
+        //        rnd = new Random(new System.DateTime().Millisecond);
+        //        string colorName = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255)).ToString();
 
-                if (Convert.ToDecimal(expenses[i].Amount) > 0.00m)
-                {
-                    Precentage = Convert.ToDecimal(expenses[i].Amount) / amount * 100;
-                }
-                PieModel pieDataInfo = new PieModel(category[i].Title, colorName, (int)Precentage);
-                pieData.Add(pieDataInfo);
-            }
-            return pieData;
-        }
+        //        if (Convert.ToDecimal(expenses[i].Amount) > 0.00m)
+        //        {
+        //            Precentage = Convert.ToDecimal(expenses[i].Amount) / amount * 100;
+        //        }
+        //        PieModel pieDataInfo = new PieModel(category[i].Title, colorName, (int)Precentage);
+        //        pieData.Add(pieDataInfo);
+        //    }
+        //    return pieData;
+        //}
 
-        double GetTotalAmount(List<Category> category)
-        {
-            double amount = 0;
-            for (int i = 0; i < category.Count; i++)
-            {
-                amount += Convert.ToDecimal(expenses[i].Amount);   
-            }
-            return amount;
-        }
+        //double GetTotalAmount(List<Category> category)
+        //{
+        //    double amount = 0;
+        //    for (int i = 0; i < category.Count; i++)
+        //    {
+        //        amount += Convert.ToDecimal(expenses[i].Amount);   
+        //    }
+        //    return amount;
+        //}
     }
 }
