7d4a10772da023f882b538843bef1584fec10524
JoeriRietjens <45871940+JoeriRietjens@users.noreply.github.com>
2020-06-17 11:49:56 +0200

    Removed progress vbar

--- View/Vue/studentbudgetapp/src/components/UserInformation.vue
+++ View/Vue/studentbudgetapp/src/components/UserInformation.vue
@@ -2,9 +2,7 @@
     <div class="collectorbody">
         <h2>{{ user.name }}</h2>
         <h2>Balance</h2> 
-        <div class="progress">
-          <progress max="100" value="70"></progress>
-        </div>
+
     </div>
 </template>
 
@@ -38,13 +36,5 @@ progress[value]::-webkit-progress-bar
     border-radius: 2px;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25) inset;
 }
-
-progress[value]::-webkit-progress-value {
-  background-image:-webkit-linear-gradient(-45deg, transparent 0%, rgba(0, 0, 0, .1) 0%, rgba(0,0, 0, .1) 0%, transparent 0%),-webkit-linear-gradient(top, rgba(255, 255, 255, .25),  rgba(0, 0, 0, .25)),-webkit-linear-gradient(left, rgb(10, 197, 110), rgb(5, 131, 72));
-
-    border-radius: 2px; 
-
-}
-
 
 </style>
