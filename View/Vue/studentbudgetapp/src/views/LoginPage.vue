38400e6ed2864dacf5532818998bf9027051986a
Bram van der Zee <bram.vdzee@gmail.com>
2020-06-17 10:00:11 +0200

    Added message for wrong login

--- View/Vue/studentbudgetapp/src/views/LoginPage.vue
+++ View/Vue/studentbudgetapp/src/views/LoginPage.vue
@@ -5,13 +5,13 @@
             <div class="form-group">
                 <label for="username">Email address</label>
                 <input type="text" v-model="username" name="username" class="form-control" :class="{ 'is-invalid': submitted && !username }" />
-                <div v-show="submitted && !username" class="invalid-feedback">Username is required</div>
+                <div v-show="submitted && !username" class="text-danger">Username is required</div>
             </div>
             <div class="form-group">
                 <label htmlFor="password">Password</label>
                 <input type="password" v-model="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && !password }" />
-                <div v-show="submitted && !password" class="invalid-feedback">Password is required</div>
-                <div v-show="wrong_login" class="invalid-feedback">Email or password is wrong.</div>
+                <div v-show="submitted && !password" class="text-danger">Password is required</div>
+                <div v-if="wrong_login" class="text-danger">Email or password is wrong.</div>
             </div>
             <div class="form-group">
                 <button class="btn btn-primary">Login</button>
@@ -29,7 +29,7 @@ export default {
         return {
             username: '',
             password: '',
-            submitted: false
+            submitted: false,
         }
     },
     computed: {
@@ -44,10 +44,9 @@ export default {
             if (username && password) {
                 this.login({ username, password })
             }
-        }
+        },
     },
     created() {
-        // reset login status
         this.logout()
     }
 }
