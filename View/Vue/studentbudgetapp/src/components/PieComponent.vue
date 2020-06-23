00f7622ca325354e3661b3e83012f9226154e779
Gino <ginovandegraaf@hotmail.com>
2020-06-23 09:29:50 +0200

    pie component update
    het werkt

--- View/Vue/studentbudgetapp/src/components/PieComponent.vue
+++ View/Vue/studentbudgetapp/src/components/PieComponent.vue
@@ -51,14 +51,5 @@ export default {
       })
 }
 
-
-
-
-// {color:'#0B6487',value:10},
-//       {color:'#9D1F37',value:40},
-//       {color:'#F6931C',value:30},
-//       {color:'#9A6487',value:10},
-//       {color:'#58A384',value:10}
-
 }
 </script>
